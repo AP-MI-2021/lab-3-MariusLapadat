1203b725f8a7fb4a1a539116af7868fb7ea17ead
MariusLapadat <lapadatmarius23@gmail.com>
2021-10-11 15:58:25 +0300

    -

--- main/main.py
+++ main/main.py
@@ -1,5 +1,76 @@
-#salut
-if __name__ == '__main__':
-    print('PyCharm')
+def printMenu():
+    print("1.Citire date")
+    print("2.Determinare cea mai lungă subsecvență cu proprietatea 5.")
+    print("Pentru a inchide, introduce x")
 
 
+def citire_lista():
+    lst = []
+    n = int(input("Dati numarul de elemente din lista: "))
+    for i in range(n):
+        lst.append(int(input("lst[" + str(i) + "]=")))
+    return lst
+
+
+def is_palindrome(n):
+    '''
+    Determina daca un numar este palindrom sau nu
+    :param n numar intreg:
+    :return True daca n este palindrom sau False daca nu:
+    '''
+    auxiliar = int(n)
+    k = 0
+    while auxiliar > 0:
+        k = k * 10 + auxiliar % 10
+        auxiliar = auxiliar // 10
+    if n == k:
+        return True
+    else:
+        return False
+
+
+def test_is_palindrome():
+    assert is_palindrome(189) is False
+    assert is_palindrome(18981) is True
+    assert is_palindrome(72) is False
+
+
+test_is_palindrome()
+
+
+def toateElementelePalindroame(lst):
+    """
+    Determina daca toate numerele dintr-o secventa a listei lst sunt palindrom
+    :param lst - lista de numere:
+    :return True sau False:
+    """
+    for x in lst:
+        if is_palindrome(x) is False:
+            return False
+    return True
+
+
+def get_longest_all_palindromes(lst: list[int]):
+    """
+    Determina cea mai lunga subsecventa de numere de tip palindrom
+    :param lst - lista de numere:
+    :return cea mai lunga subsecventa de numere palindrom din lst:
+    """
+    subsecventaMax = []
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if toateElementelePalindroame(lst[i:j + 1]) and len(lst[i:j + 1]) > len(subsecventaMax):
+                subsecventaMax = lst[i:j + 1]
+    return subsecventaMax
+
+
+while True:
+    printMenu()
+    optiune = input("Dati optiunea: ")
+    if optiune == "1":
+        l = citire_lista()
+    elif optiune == "2":
+        k = get_longest_all_palindromes(l)
+        print(k[:])
+    elif optiune == "x":
+        break
